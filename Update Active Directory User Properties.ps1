abdd83520dd86f0aec09eb596efc1ae92a61cd42
Michael Seidl | au2mator <michael.seidl@au2mator.com>
2022-01-24 14:06:27 +0100

    30 more

--- Update Active Directory User Properties.ps1
+++ Update Active Directory User Properties.ps1
@@ -381,13 +381,413 @@ Get-ADUser -Identity TecMi -Properties mobile | Select-Object -Property mobile
 
 
 
+##name
+##
+#SET Values
+Get-ADUser -Identity TecMi | Rename-ADObject -NewName "Michael Techguy" 
+
+#CLEAR Value
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties name | Select-Object -Property name
 
 
 
+##o
+##
+#SET Values
+Set-ADUser -Identity TecMi  -Replace @{o="au2mator"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear o
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties o | Select-Object -Property o
 
 
 
-
-
-
+##otherFacsimileTelephoneNumber
+##
+#SET Values
 Set-ADUser -Identity TecMi -Replace @{otherFacsimileTelephoneNumber=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherFacsimileTelephoneNumber=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherFacsimileTelephoneNumber=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherFacsimileTelephoneNumber
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherFacsimileTelephoneNumber | Select-Object -Property otherFacsimileTelephoneNumber
+
+
+
+##otherHomePhone
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{otherHomePhone=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherHomePhone=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherHomePhone=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherHomePhone
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherHomePhone | Select-Object -Property otherHomePhone
+
+
+
+##otherIpPhone
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{otherIpPhone=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherIpPhone=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherIpPhone=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherIpPhone
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherIpPhone | Select-Object -Property otherIpPhone
+
+
+
+##otherMobile
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{otherMobile=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherMobile=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherMobile=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherMobile
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherMobile | Select-Object -Property otherMobile
+
+
+
+##otherPager
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{otherPager=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherPager=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherPager=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherPager
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherPager | Select-Object -Property otherPager
+
+
+##otherTelephone
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{otherTelephone=@("+123", "456", "+789")}
+Set-ADUser -Identity TecMi -Add @{otherTelephone=@("656566")}
+Set-ADUser -Identity TecMi -Remove @{otherTelephone=@("656566")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear otherTelephone
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties otherTelephone | Select-Object -Property otherTelephone
+
+
+
+##pager
+##
+#SET Values
+Set-ADUser -Identity TecMi  -Replace @{pager="12334534"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear pager
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties pager | Select-Object -Property pager
+
+
+
+##personalTitle
+##
+#SET Values
+Set-ADUser -Identity TecMi  -Replace @{personalTitle="Dr"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear personalTitle
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties personalTitle | Select-Object -Property personalTitle
+
+
+
+##photo
+##
+#SET Values
+$photo = [byte[]](Get-Content -Path D:\HEad.png -AsByteStream)
+Set-ADUser -Identity TecMi -Replace @{photo=$photo} 
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear photo
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties photo | Select-Object -Property photo
+
+
+
+
+##physicalDeliveryOfficeName
+##
+#SET Values
+Set-ADUser -Identity TecMi  -Replace @{physicalDeliveryOfficeName="Floor 2"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear physicalDeliveryOfficeName
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties physicalDeliveryOfficeName | Select-Object -Property physicalDeliveryOfficeName
+
+
+##postalAddress
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{postalAddress=@("Street 2, 4044 Place")}
+Set-ADUser -Identity TecMi -Add @{postalAddress=@("Street 1, 4044 Place")}
+Set-ADUser -Identity TecMi -Remove @{postalAddress=@("Street 1, 4044 Place")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear postalAddress
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties postalAddress | Select-Object -Property postalAddress
+
+
+
+##postalCode
+##
+#SET Values
+Set-ADUser -Identity TecMi  -PostalCode "4540"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear postalCode
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties postalCode | Select-Object -Property postalCode
+
+
+##postOfficeBox
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{postOfficeBox=@("Street 2, 4044 Place")}
+Set-ADUser -Identity TecMi -Add @{postOfficeBox=@("Street 1, 4044 Place")}
+Set-ADUser -Identity TecMi -Remove @{postOfficeBox=@("Street 1, 4044 Place")}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear postOfficeBox
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties postOfficeBox | Select-Object -Property postOfficeBox
+
+
+
+
+##preferredLanguage
+##
+#SET Values
+Set-ADUser -Identity TecMi  -Replace @{preferredLanguage="DE-AT"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear preferredLanguage
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties preferredLanguage | Select-Object -Property preferredLanguage
+
+
+
+
+##primaryGroupID
+##
+#SET Values
+$ADGroup=Get-ADGroup -Identity "Test Primary"
+Add-ADGroupMember -Identity $ADGroup.DistinguishedName -Members "TecMi"
+$primaryGroupID = $ADGroup.SID -replace @('.+\-','')
+Set-ADUser -Identity TecMi -Replace @{'primaryGroupID' = $primaryGroupID} -ErrorAction SilentlyContinue -PassThru 
+
+#CLEAR Value
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties primaryGroupID | Select-Object -Property primaryGroupID
+
+
+
+
+
+##primaryInternationalISDNNumber
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{primaryInternationalISDNNumber="0123456"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear primaryInternationalISDNNumber
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties primaryInternationalISDNNumber | Select-Object -Property primaryInternationalISDNNumber
+
+
+
+##primaryTelexNumber
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{primaryTelexNumber="0123456"}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear primaryTelexNumber
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties primaryTelexNumber | Select-Object -Property primaryTelexNumber
+
+
+
+##profilePath
+##
+#SET Values
+Set-ADUser -Identity TecMi -ProfilePath "\\server1\share\User"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear ProfilePath
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties ProfilePath | Select-Object -Property ProfilePath
+
+
+
+##sAMAccountName
+##
+#SET Values
+Set-ADUser -Identity TecMi -SamAccountName "TecMi"
+
+#CLEAR Value
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties SamAccountName | Select-Object -Property SamAccountName
+
+
+
+##scriptPath
+##
+#SET Values
+Set-ADUser -Identity TecMi -ScriptPath "\\server1\share\User\logon.bat"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear ScriptPath
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties ScriptPath | Select-Object -Property ScriptPath
+
+
+
+##secretary
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{secretary=@((Get-ADuser -Identity "m.seidl").DistinguishedName)}
+Set-ADUser -Identity TecMi -Add  @{secretary=@((Get-ADuser -Identity "HofmeisterJ").DistinguishedName)}
+Set-ADUser -Identity TecMi -Remove  @{secretary=@((Get-ADuser -Identity "m.seidl").DistinguishedName)}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear secretary
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties secretary | Select-Object -Property secretary
+
+
+##seeAlso
+##
+#SET Values
+Set-ADUser -Identity TecMi -Replace @{seeAlso=@((Get-ADuser -Identity "m.seidl").DistinguishedName)}
+Set-ADUser -Identity TecMi -Add  @{seeAlso=@((Get-ADuser -Identity "HofmeisterJ").DistinguishedName)}
+Set-ADUser -Identity TecMi -Remove  @{seeAlso=@((Get-ADuser -Identity "m.seidl").DistinguishedName)}
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear seeAlso
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties seeAlso | Select-Object -Property seeAlso
+
+
+
+##sn
+##
+#SET Values
+Set-ADUser -Identity TecMi -Surname "Seidl"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear sn
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties sn | Select-Object -Property sn
+
+
+##street
+##
+#SET Values
+Set-ADUser -Identity TecMi -StreetAddress "Wilhelm-Fein-Straﬂe 37"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear StreetAddress
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties StreetAddress | Select-Object -Property StreetAddress
+
+
+##telephoneNumber
+##
+#SET Values
+Set-ADUser -Identity TecMi -OfficePhone "+4 364 6 6 465"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear telephoneNumber
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties OfficePhone | Select-Object -Property OfficePhone
+
+
+
+
+##title
+##
+#SET Values
+Set-ADUser -Identity TecMi -Title "CEO"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear title
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties title | Select-Object -Property title
+
+
+
+##userPrincipalName
+##
+#SET Values
+Set-ADUser -Identity TecMi -UserPrincipalName "TecMi@au2mator.local"
+
+#CLEAR Value
+
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties UserPrincipalName | Select-Object -Property UserPrincipalName
+
+
+
+
+
+##wWWHomePage
+##
+#SET Values
+Set-ADUser -Identity TecMi -HomePage "www.au2mator.com"
+
+#CLEAR Value
+Set-ADUser -Identity TecMi -Clear wWWHomePage
+
+#READ Value
+Get-ADUser -Identity TecMi -Properties HomePage | Select-Object -Property HomePage
